a77d1212cf9ba6f2ef7beb6a5a9b4f2580304887
macadelicsupreme <macadelicsupeme@outlook.com>
2024-01-25 17:20:50 -0500

    first commit

---
+++ src/main/java/com/blog/maccodes/MaccodesApplication.java
@@ -0,0 +1,13 @@
+package com.blog.maccodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaccodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaccodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/maccodes/MaccodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.maccodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaccodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
